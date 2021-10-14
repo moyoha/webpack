309c31893310f81a0faaf4ee994dd253c5392762
mayoha <mayoha@163.com>
2021-10-14 15:09:22 +0800

    转义js

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,27 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode:'development', // 开发模式
+  mode: 'development', // 开发模式
   entry: {
-    main: path.resolve(__dirname,'../src/main.js'),
-    share: path.resolve(__dirname,'../src/share.js')
+    main: path.resolve(__dirname, '../src/main.js'),
+    share: path.resolve(__dirname, '../src/share.js')
   },
   output: {
-      filename: '[name].[hash:8].js', // [name] 指entry属性名字, 默认为main
-      path: path.resolve(__dirname,'../dist') // 打包后的目录
+    filename: '[name].[hash:8].js', // [name] 指entry属性名字, 默认为main
+    path: path.resolve(__dirname, '../dist') // 打包后的目录
   },
-  plugins:[
+  plugins: [
     new HtmlWebpackPlugin({
       // 选取一个html作为模版，在dist目录下会生成一个相同的html，之后将打包好的js注入到该html文件
-      template: path.resolve(__dirname,'../public/index.html'),
+      template: path.resolve(__dirname, '../public/index.html'),
       filename: 'index.html',
       chunks: ['main'] // 配置将哪些文件注入到该html文件，为[]在代表不注入js，若无该属性，默认注入所有js
     }),
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname,'../public/share.html'),
+      template: path.resolve(__dirname, '../public/share.html'),
       filename: 'share.html',
       chunks: ['share']
     }),
@@ -41,8 +41,8 @@ module.exports = {
         use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"] // 此外还需要安装node-sass(sass也行)
       },
       {
-        test:/\.less$/,
-        use:[MiniCssExtractPlugin.loader,'css-loader', "postcss-loader", 'less-loader'] // 此外还需要安装less模块
+        test: /\.less$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', "postcss-loader", 'less-loader'] // 此外还需要安装less模块
       },
       {
         test: /\.(jpe?g|png|gif)$/i,
@@ -54,7 +54,36 @@ module.exports = {
       {
         test: /\.html$/i,
         loader: "html-loader"
-      }
+      },
+      {
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              [
+                '@babel/preset-env',
+                {
+                  useBuiltIns: 'usage',
+                  corejs: {
+                    //core-js的版本
+                    version: 3
+                  },
+                  //需要兼容的浏览器
+                  targets: {
+                    chrome: '60',
+                    firefox: '60',
+                    ie: '9',
+                    safari: '10',
+                    edge: '17'
+                  }
+                }
+              ]
+            ],
+          }
+        },
+        exclude: /node_modules/
+      },
     ]
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,3 +4,14 @@ import "../static/css/fontsize.less"
 import GitImg from '../static/img/git.jpeg'
 
 console.log("webpack")
+let person = {
+  msg: {
+    name: "mayoha"
+  }
+}
+
+let name = person?.msg?.name
+console.log(name)
+
+let set = new Set([1,2,1])
+console.log(set)
